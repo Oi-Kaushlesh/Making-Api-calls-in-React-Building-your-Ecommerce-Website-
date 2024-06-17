0432cbdf06ee9cfc59efb1ded700bdb09948b15b
Oi-Kaushlesh <kaushlesh2611singh@gmail.com>
2024-06-17 17:22:35 +0530

    Loading the application

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import React,{useState} from 'react';
+import React, { useState } from "react";
 
-import MoviesList from './components/MoviesList';
-import './App.css';
+import MoviesList from "./components/MoviesList";
+import "./App.css";
+import spinner from "./spinner.gif";
 
 function App() {
-  const [movies, setMovies] = useState([])
+  const [loaderStatus, setLoaderStatus] = useState(false);
+  const [movies, setMovies] = useState([]);
 
   async function fetchMovieHandler() {
-    const response = await fetch('https://swapi.py4e.com/api/films/');
+    setLoaderStatus(true);
+    const response = await fetch("https://swapi.py4e.com/api/films/");
     const data = await response.json();
     const transformedMovie = data.results.map((movieData) => {
       return {
-        id:movieData.episode_id,
-        title : movieData.title,
-        openingText : movieData.opening_crawl,
-        releaseDate : movieData.release_date
-      } 
-    })
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date,
+      };
+    });
+    setLoaderStatus(false);
     setMovies(transformedMovie);
   }
   return (
@@ -25,7 +29,17 @@ function App() {
         <button onClick={fetchMovieHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!loaderStatus && <MoviesList movies={movies} />}
+        {loaderStatus && (
+          <div class="text-center">
+            <img
+              src={spinner}
+              class="rounded"
+              alt="loading"
+              style={{ width: "60px", height: "70px" }}
+            />
+          </div>
+        )}
       </section>
     </React.Fragment>
   );
